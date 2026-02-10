92b1cbc2bb28ae8381aeadb658f187b319decdd6
tuvit <tuvitk@wix.com>
2026-02-10 21:40:36 +0200

    Initial commit from Create Wix App

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: '09473501-08c4-4d75-ad92-b1c66c3905dd',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+});
